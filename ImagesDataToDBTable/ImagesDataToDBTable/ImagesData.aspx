141fcf799e40c61a99c5025014b584cb68e6ab50
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-10-02 11:22:50 +0530

    v1

---
+++ ImagesDataToDBTable/ImagesDataToDBTable/ImagesData.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImagesData.aspx.cs" Inherits="ImagesDataToDBTable.ImagesData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            font-size: x-large;
+            color: #FFFFFF;
+        }
+    </style>
+</head>
+<body>
+    <h1>Uploading Images Data to DataBase Table</h1>
+    <p>&nbsp;</p>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:FileUpload ID="ImageFileUpload" runat="server" CssClass="auto-style1" />
+            <br class="auto-style1" />
+            <br class="auto-style1" />
+            <asp:Button ID="btnAdd" runat="server" BackColor="Blue" CssClass="auto-style2" OnClick="btnAdd_Click" Text="Add" />
+            <br />
+            <br />
+            <br />
+            <asp:Image ID="Image1" runat="server" Width="600px" Height="400px" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" CssClass="auto-style1" ForeColor="Green"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ImagesDataToDBTable/ImagesDataToDBTable/ImagesData.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace ImagesDataToDBTable
+{
+    public partial class ImagesData : System.Web.UI.Page
+    {
+        //Establishing Conection to Sql Server
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["IamgeConnection"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Image1.Visible = false;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ImageFileUpload.HasFile)
+            {
+                try
+                {
+                    byte[] ImageData;
+                    //Creating a file stream object to open the image file that is uploaded 
+                    using (Stream fs = ImageFileUpload.PostedFile.InputStream)
+                    {
+                        //creating a binary reader object to read binary data of image
+                        using (BinaryReader br = new BinaryReader(fs))
+                        {
+                            ImageData = br.ReadBytes(Convert.ToInt32(fs.Length));
+                        }
+                        string ImageName = Path.GetFileName(ImageFileUpload.FileName);
+                        AddIamges(ImageName , ImageData);
+                        lblMessage.Text = "Image Added Successfully.";
+
+                        //Displaying the uploaded image
+                        string Base64String = Convert.ToBase64String(ImageData , 0 , ImageData.Length);  //Converting byte array to base64string
+                        //Creating a data url for the image
+                        Image1.ImageUrl = "data:image/jpg;base64," + Base64String;
+                        Image1.Visible = true;
+                    }
+                }
+                catch (Exception ex1)
+                {
+                    lblMessage.Text = ex1.Message;
+                }
+            }
+            else
+                lblMessage.Text = "Image not uploaded! Please upload an image first";
+        }
+
+        public void AddIamges(string imgname,byte[] imgdata)
+        {
+            try
+            {
+                connection.Open();
+                string query = "INSERT INTO IMAGES(IMGNAME,IMGDATA) VALUES (@imgname,@imgdata)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@imgname", imgname);
+                cmd.Parameters.AddWithValue("@imgdata", imgdata);
+                int res = cmd.ExecuteNonQuery();
+                if (res == 1)
+                    lblMessage.Text = "Image added to database successfully...";
+                else
+                    lblMessage.Text = "Image not added to database!";
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+}
